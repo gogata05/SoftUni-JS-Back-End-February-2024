824a4994c9f8382bdb51e3cf383a960a7b2471ca
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-01-07 12:06:55 +0200

    up
    up

--- scaffold Scr copy n paste/src/controller/homeController.js
+++ scaffold Scr copy n paste/src/controller/homeController.js
@@ -7,6 +7,7 @@ const itemServices = require('../services/itemServices');
 const { isAuth } = require('../middleware/authMiddleware');
 router.get('/', (req, res) => {
     // let getTop = await itemServices.getTopThree().lean();//last 3 posts to home
+    // let allPost = await itemServices.getAll();
     res.render('home');
 });
 
